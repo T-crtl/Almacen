b8de0014988daf81ae735ee0d27e3258b50a5557
DavidGtz <david.gutierrez.dt@gmail.com>
2023-08-23 19:14:10 -0600

    Include de bootstrap en ('app.blade')

--- almacen/resources/views/layouts/app.blade.php
+++ almacen/resources/views/layouts/app.blade.php
@@ -13,9 +13,18 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+    <!-- MODIFICACION INCLUDE BOOTSTRAP - DAVID -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <!-- @vite(['almacen/resources/sass/app.scss', 'almacen/resources/js/app.js']) -->
+    <!-- MODIFICACION INCLUDE BOOTSTRAP - DAVID -->
+    @yield('contenido')
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" 
+    crossorigin="anonymous"></script>
+    
 </head>
 
 <body>
