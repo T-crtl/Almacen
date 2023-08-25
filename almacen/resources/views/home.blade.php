fafc975867434a78b1462bbb15ae81c8f79be4d3
Abraham <cesarduran302@gmail.com>
2023-08-24 20:09:31 -0600

    add frontend consultas e inicio

--- almacen/resources/views/home.blade.php
+++ almacen/resources/views/home.blade.php
@@ -1,23 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                <h1>Consulta de datos</h1>
+            
+                <div class="container">
+                    <form action="consulta.php" method="post">
+                        <label for="tipo_consulta">Tipo de consulta:</label>
+                        <select name="tipo_consulta" id="tipo_consulta">
+                            <option value="area">Área</option>
+                            <option value="articulos">Artículos</option>
+                            <option value="division">División</option>
+                            <option value="traslado">Traslado</option>
+                            <option value="resguardo">Resguardo</option>
+                        </select>
+                        <br>
+                        <input type="submit" value="Buscar">
+                    </form>
+                    
                 </div>
+
             </div>
         </div>
     </div>
-</div>
 @endsection
