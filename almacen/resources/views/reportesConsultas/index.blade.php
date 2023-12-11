fc6397e542817204325f52c3e331c706ee661384
Abraham <cesarduran302@gmail.com>
2023-12-10 23:22:30 -0600

    test

--- almacen/resources/views/reportesConsultas/index.blade.php
+++ almacen/resources/views/reportesConsultas/index.blade.php
@@ -36,14 +36,6 @@
             </div>
         </div>
 
-        <!-- Page level plugins -->
-<script src="https://cdn.ceti.mx/siic/v2/js/Chart.js"></script>
 <!-- Page level custom scripts -->
-<script src="https://cdn.ceti.mx/siic/v2/js/chart-area-demo.js"></script>
-<script src="https://cdn.ceti.mx/siic/v2/js/chart-pie-demo.js"></script>
-<script src="https://cdn.ceti.mx/siic/v2/js/imask.js"></script>
-<script src="https://cdn.ceti.mx/siic/v2/js/sweetalert2.all.js"></script>
 <script src="https://cdn.ceti.mx/siic/v2/js/siic.js"></script>
-<script src="https://cdn.ceti.mx/siic/v2/js/reloj.js"></script>
 <script src="https://cdn.ceti.mx/siic/v2/js/app-siic1.0.0.js"></script>
-<script src="https://cdn.ceti.mx/siic/v2/js/app-contextMenu.js"></script>
