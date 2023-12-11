f15afcd69de7e223275217d92e4561040e9f3114
David <david.gutierrez.dt@gmail.com>
2023-12-10 22:04:45 -0600

    Ajustes iconos, Front.

--- almacen/resources/views/home.blade.php
+++ almacen/resources/views/home.blade.php
@@ -9,8 +9,11 @@
                     <div class="card-header text-center">
                         Reportes y Consultas
                     </div>
-                        <div class="card-body" style="background-color: #035374;-webkit-mask: url(icons/57.svg) no-repeat center;mask: url(icons/57.svg) no-repeat center;margin: 35px;-webkit-mask-size: contain;">
-                            
+                        <div class="card-body" style="mask: url(icons/57.svg) no-repeat center;margin: 15px;-webkit-mask-size: contain;">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="#035374" class="bi bi-card-text" viewBox="0 0 16 16">
+                                <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2z"/>
+                                <path d="M3 5.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5M3 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 8m0 2.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5"/>
+                              </svg>
                         </div>
                     </div>
                 </div>
@@ -20,8 +23,15 @@
                         <div class="card-header text-center">
                             Base de datos
                         </div>
-                        <div class="card-body" style="background-color: #CCCCCC; -webkit-mask: url(icons/otra_icono.svg) no-repeat center; mask: url(icons/otra_icono.svg) no-repeat center; margin: 35px; -webkit-mask-size: contain;">
-                            
+                        <!--<div class="card-body" style="background-color: #CCCCCC; -webkit-mask: url(icons/otra_icono.svg) no-repeat center; mask: url(icons/otra_icono.svg) no-repeat center; margin: 35px; -webkit-mask-size: contain;">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-database" viewBox="0 0 16 16">
+                                <path d="M4.318 2.687C5.234 2.271 6.536 2 8 2s2.766.27 3.682.687C12.644 3.125 13 3.627 13 4c0 .374-.356.875-1.318 1.313C10.766 5.729 9.464 6 8 6s-2.766-.27-3.682-.687C3.356 4.875 3 4.373 3 4c0-.374.356-.875 1.318-1.313ZM13 5.698V7c0 .374-.356.875-1.318 1.313C10.766 8.729 9.464 9 8 9s-2.766-.27-3.682-.687C3.356 7.875 3 7.373 3 7V5.698c.271.202.58.378.904.525C4.978 6.711 6.427 7 8 7s3.022-.289 4.096-.777A4.92 4.92 0 0 0 13 5.698M14 4c0-1.007-.875-1.755-1.904-2.223C11.022 1.289 9.573 1 8 1s-3.022.289-4.096.777C2.875 2.245 2 2.993 2 4v9c0 1.007.875 1.755 1.904 2.223C4.978 15.71 6.427 16 8 16s3.022-.289 4.096-.777C13.125 14.755 14 14.007 14 13zm-1 4.698V10c0 .374-.356.875-1.318 1.313C10.766 11.729 9.464 12 8 12s-2.766-.27-3.682-.687C3.356 10.875 3 10.373 3 10V8.698c.271.202.58.378.904.525C4.978 9.71 6.427 10 8 10s3.022-.289 4.096-.777A4.92 4.92 0 0 0 13 8.698m0 3V13c0 .374-.356.875-1.318 1.313C10.766 14.729 9.464 15 8 15s-2.766-.27-3.682-.687C3.356 13.875 3 13.373 3 13v-1.302c.271.202.58.378.904.525C4.978 12.71 6.427 13 8 13s3.022-.289 4.096-.777c.324-.147.633-.323.904-.525"/>
+                              </svg>
+                        </div>-->
+                        <div class="card-body" style="mask: url(icons/otra_icono.svg) no-repeat center; margin: 15px; -webkit-mask-size: contain;">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="#035374" class="bi bi-database" viewBox="0 0 16 16">
+                                <path d="M4.318 2.687C5.234 2.271 6.536 2 8 2s2.766.27 3.682.687C12.644 3.125 13 3.627 13 4c0 .374-.356.875-1.318 1.313C10.766 5.729 9.464 6 8 6s-2.766-.27-3.682-.687C3.356 4.875 3 4.373 3 4c0-.374.356-.875 1.318-1.313ZM13 5.698V7c0 .374-.356.875-1.318 1.313C10.766 8.729 9.464 9 8 9s-2.766-.27-3.682-.687C3.356 7.875 3 7.373 3 7V5.698c.271.202.58.378.904.525C4.978 6.711 6.427 7 8 7s3.022-.289 4.096-.777A4.92 4.92 0 0 0 13 5.698M14 4c0-1.007-.875-1.755-1.904-2.223C11.022 1.289 9.573 1 8 1s-3.022.289-4.096.777C2.875 2.245 2 2.993 2 4v9c0 1.007.875 1.755 1.904 2.223C4.978 15.71 6.427 16 8 16s3.022-.289 4.096-.777C13.125 14.755 14 14.007 14 13zm-1 4.698V10c0 .374-.356.875-1.318 1.313C10.766 11.729 9.464 12 8 12s-2.766-.27-3.682-.687C3.356 10.875 3 10.373 3 10V8.698c.271.202.58.378.904.525C4.978 9.71 6.427 10 8 10s3.022-.289 4.096-.777A4.92 4.92 0 0 0 13 8.698m0 3V13c0 .374-.356.875-1.318 1.313C10.766 14.729 9.464 15 8 15s-2.766-.27-3.682-.687C3.356 13.875 3 13.373 3 13v-1.302c.271.202.58.378.904.525C4.978 12.71 6.427 13 8 13s3.022-.289 4.096-.777c.324-.147.633-.323.904-.525"/>
+                              </svg>
                         </div>
                     </div>
                 </div>
--- almacen/resources/views/reportesConsultas/index.blade.php
+++ almacen/resources/views/reportesConsultas/index.blade.php
@@ -1,41 +1,34 @@
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div>
-                    @if (Auth::check())
-                        
-                        <!--RESGUARDOS CARD -->
-                        <div class="col-md-3 getModulos" data-ruta="{{ route('consultas.index') }}" data-method="get"
-                            style="margin-top:20px;">
-                            <div class="card text-center module">
-                                <div class="card-header text-center">
-                                    Resguardos
-                                </div>
-                                <div class="card-body"
-                                    style="background-color: #CCCCCC; -webkit-mask: url(icons/otra_icono.svg) no-repeat center; mask: url(icons/otra_icono.svg) no-repeat center; margin: 35px; -webkit-mask-size: contain;">
-
-                                </div>
+@if (Auth::check())
+    <div class="row" id="work_area">
+        <div class="container h-85 moduloActivo" style="overflow: auto;" id="container-area">
+            <div class="row h-50" id="work_area">
+                <!--RESGUARDOS CARD -->
+                <div class="col-md-3 getModulos" data-ruta="{{ route('consultas.index') }}" data-method="get" style="margin-top:20px;">
+                    <div class="card text-center module">
+                            <div class="card-header text-center">
+                                Resguardos
+                            </div>
+                            <div class="card-body"
+                                style="background-color: #CCCCCC; -webkit-mask: url(icons/otra_icono.svg) no-repeat center; mask: url(icons/otra_icono.svg) no-repeat center; margin: 35px; -webkit-mask-size: contain;">
                             </div>
                         </div>
+                    </div>
                         <!--Traslados CARD -->
-                        <div class="col-md-3 getModulos" data-ruta="{{ route('trasladoArticulo.index') }}" data-method="get"
+                    <div class="col-md-3 getModulos" data-ruta="{{ route('trasladoArticulo.index') }}" data-method="get"
                             style="margin-top:20px;">
-                            <div class="card text-center module">
-                                <div class="card-header text-center">
-                                    Traslados
-                                </div>
-                                <div class="card-body"
-                                    style="background-color: #CCCCCC; -webkit-mask: url(icons/otra_icono.svg) no-repeat center; mask: url(icons/otra_icono.svg) no-repeat center; margin: 35px; -webkit-mask-size: contain;">
-
-                                </div>
+                        <div class="card text-center module">
+                            <div class="card-header text-center">
+                                Traslados
+                            </div>
+                            <div class="card-body"
+                                style="background-color: #CCCCCC; -webkit-mask: url(icons/otra_icono.svg) no-repeat center; mask: url(icons/otra_icono.svg) no-repeat center; margin: 35px; -webkit-mask-size: contain;">
                             </div>
                         </div>
-                    @endif
-
+                    </div>
+                @endif
                 </div>
             </div>
         </div>
-
         <!-- Page level plugins -->
 <script src="https://cdn.ceti.mx/siic/v2/js/Chart.js"></script>
 <!-- Page level custom scripts -->
